9b2db5548cc3e6ebb1c93aed5cd1d02d65be5cdb
bloodlqst <cooper.gallibu@students.mq.edu.au>
2025-05-30 22:04:39 +1000

    Warrior placeholders

--- Assets/Tilesheet/tilesheet.tsx
+++ Assets/Tilesheet/tilesheet.tsx
@@ -340,9 +340,19 @@
    <object id="2" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="54">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="16"/>
+  </objectgroup>
+ </tile>
  <tile id="56">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="1" y="0" width="13" height="16"/>
+   <object id="2" x="1" y="0" width="13" height="16"/>
   </objectgroup>
  </tile>
  <tile id="57">
